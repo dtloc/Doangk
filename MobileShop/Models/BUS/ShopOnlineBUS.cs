45b9ccdb036b89a27cee26716b51a8f7aca01c35
DESKTOP-6SP2ENS\Lộc Siêu Nhân <locdt.fsa@gmail.com>
2017-04-20 16:26:49 +0700

    Load danh sach san pham , chi tiet san pham, them images

---
+++ MobileShop/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,23 @@
+﻿using PlMoblieConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<sanpham> DanhSach()
+        {
+            var db = new PlMoblieConnectionDB();
+            return db.Query<sanpham>("select * from sanpham where bixoa =0");
+        }
+        public static PlMoblieConnection.sanpham ChiTietSanPham(int id)
+        {
+            var ct = new PlMoblieConnectionDB();
+            return ct.SingleOrDefault<PlMoblieConnection.sanpham>("select * from sanpham where MaSanPham=@0", id);
+
+        }
+    }
+}
